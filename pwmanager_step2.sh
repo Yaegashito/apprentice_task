6f555660ca128798ffe573ddb668edfe671bd059
tomoya <tomoyae1007.xperia26@gmail.com>
2024-02-20 10:30:03 +0900

    Get Password内を追記し、Step2完成

--- pwmanager_step2.sh
+++ pwmanager_step2.sh
@@ -7,22 +7,24 @@ do
 read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" answer
 
 if [ "$answer" = "Add Password" ]; then
-    	read -p "サービス名を入力してください：" service_name
-    	read -p "ユーザー名を入力してください：" user_name
-    	read -p "パスワードを入力してください：" password
-    	echo "$service_name:$user_name:$password" >> "pwdata_step2.txt"
-    	echo "パスワードの追加は成功しました。"
+    read -p "サービス名を入力してください：" service_nameA
+    read -p "ユーザー名を入力してください：" user_name
+    read -p "パスワードを入力してください：" password
+    echo "$service_nameA:$user_name:$password" >> "pwdata_step2.txt"
+    echo "パスワードの追加は成功しました。"
 elif [ "$answer" = "Get Password" ]; then
-    	read -p "サービス名を入力してください：" service_name2
-    	if [ -n "$service_name2" ]; then
-            	echo "なにかのサービスは登録されています"
-    	elif [ -z "$service_name2" ]; then
+    read -p "サービス名を入力してください：" service_nameG
+    	if [ -z $(grep "^$service_nameG:" pwdata_step2.txt) ]; then
             	echo "登録なし"
+  		elif [ -n $(grep "^$service_nameG:" pwdata_step2.txt) ]; then
+   	 			echo "サービス名：$service_nameG"
+   	 			echo "ユーザー名：$(grep "^$service_nameG:" pwdata_step2.txt | cut -d ":" -f 2)"
+   	 			echo "パスワード：$(grep "^$service_nameG:" pwdata_step2.txt | cut -d ":" -f 3)"
     	fi
 elif [ "$answer" = "Exit" ]; then
-    	echo "Thank you!"
-    	break
+    echo "Thank you!"
+    break
 else
-    	echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+    echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
 fi
 done
